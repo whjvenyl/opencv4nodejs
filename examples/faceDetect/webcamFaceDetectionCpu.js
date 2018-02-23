b822a3996f08125059fc7f03339790e3742bff4f
Toni Pesut <toni.pesut@jls.ch>
2018-02-23 16:39:03 +0100

    Add ability to set max fps to reduce load on cpu

--- examples/faceDetect/webcamFaceDetectionCpu.js
+++ examples/faceDetect/webcamFaceDetectionCpu.js
@@ -7,11 +7,13 @@ const { runVideoFaceDetection } = require('./commons');
 const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
 
 const webcamPort = 0;
+const MINSIZE = 40;
 
 function detectFaces(img) {
   // restrict minSize and scaleFactor for faster processing
   const options = {
-    minSize: new cv.Size(5, 5),
+    // minSize: new cv.Size(20, 20), // default, more cpu intensive
+    minSize: new cv.Size(MINSIZE, MINSIZE),
     scaleFactor: 1.2,
     // scaleFactor: 1.5,
     minNeighbors: 10
