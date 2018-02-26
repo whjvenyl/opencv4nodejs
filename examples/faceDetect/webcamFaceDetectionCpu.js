254d38b89bcd663c961ff3787ec56e3fe3dbbf03
Toni Pesut <toni.pesut@jls.ch>
2018-02-26 10:00:37 +0100

    Remove unneeded files

--- examples/faceDetect/webcamFaceDetectionCpu.js
+++ examples/faceDetect/webcamFaceDetectionCpu.js
@@ -1,22 +1,23 @@
 const {
-  cv
+    cv
 } = require('../utils');
 
-const { runVideoFaceDetection } = require('./commons');
+const {runVideoFaceDetection} = require('./commons');
 
 const classifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_ALT2);
 
 const webcamPort = 0;
 const MINSIZE = 40;
+
 function detectFaces(img) {
-  // restrict minSize and scaleFactor for faster processing
-  const options = {
-    minSize: new cv.Size(MINSIZE, MINSIZE),
-    scaleFactor: 1.2,
-    // scaleFactor: 1.5,
-    minNeighbors: 10
-  };
-  return classifier.detectMultiScale(img.bgrToGray(), options).objects;
+    // restrict minSize and scaleFactor for faster processing
+    const options = {
+        minSize: new cv.Size(MINSIZE, MINSIZE),
+        scaleFactor: 1.2,
+        // scaleFactor: 1.5,
+        minNeighbors: 10
+    };
+    return classifier.detectMultiScale(img.bgrToGray(), options).objects;
 }
 
 runVideoFaceDetection(webcamPort, detectFaces);
